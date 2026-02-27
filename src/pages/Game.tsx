d0d96dc1d6a5d7930a50b9e43ff221d03fb0222f
sabra <sabrasara2002@gmail.com>
2026-02-27 13:42:22 +0100

    chore: remove test ping and reset pages scaffold

---
+++ src/pages/Game.tsx
@@ -0,0 +1,8 @@
+export default function Game() {
+    return (
+        <main style={{ padding: 16 }}>
+            <h1>Game</h1>
+            <p>Hier kommt später das Press & Regret Spiel rein.</p>
+        </main>
+    );
+}
