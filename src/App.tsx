3191f565f1e4db5eb3eb3d6f020bee53ec661eaf
sabra <sabrasara2002@gmail.com>
2026-03-17 11:13:37 +0100

    Schwierigkeiten ergänzt

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import HowToPlay from "./pages/HowToPlay";
 function Nav() {
     return (
         <nav style={{ display: "flex", gap: 12, padding: 16, borderBottom: "1px solid #ddd" }}>
-            <Link to="/Game">Game</Link>
+            <Link to="/game">Game</Link>
+            <Link to="/game/easy">Game Easy</Link>
+            <Link to="/game/medium">Game Medium</Link>
+            <Link to="/game/hard">Game Hard</Link>
             <Link to="/how-to-play">How to play</Link>
             <Link to="/about">About</Link>
         </nav>
@@ -18,7 +21,10 @@ export default function App() {
         <BrowserRouter>
             <Nav />
             <Routes>
-                <Route path="/Game" element={<Game />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/game/easy" element={<Game />} />
+                <Route path="/game/medium" element={<Game />} />
+                <Route path="/game/hard" element={<Game />} />
                 <Route path="/how-to-play" element={<HowToPlay />} />
                 <Route path="/about" element={<About />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,6 +1,10 @@
 import Easy from "./Levels/Easy";
+import Medium from "./Levels/Medium";
+import Hard from "./Levels/Hard";
 import { useEffect, useState } from "react";
+import {useParams, Navigate} from "react-router-dom";
 import { api } from "../api/http";
+
 
 type LevelResponse = {
     levelId: number;
@@ -10,16 +14,32 @@ type LevelResponse = {
 };
 
 export default function Game() {
+    const { difficulty } = useParams();
     const [levels, setLevels] = useState<LevelResponse[] | null>(null);
 
+    const difficultyMap: Record<string, number> = {
+        easy: 1,
+        medium: 2,
+        hard: 3,
+    }
+    const difficultyId = difficulty ? difficultyMap[difficulty] : undefined;
+
     useEffect(() => {
-        api.get<LevelResponse[]>("/api/difficulties/1/levels")
+        if(!difficultyId) return;
+
+        setLevels(null);
+
+        api.get<LevelResponse[]>(`/api/difficulties/${difficultyId}/levels`)
             .then((r) => {
                 console.log(r.data);
                 setLevels(r.data);
             })
             .catch((err) => console.error(err));
-    }, []);
+    }, [difficultyId]);
+
+    if(!difficultyId){
+        return <Navigate to="/game/easy" replace />;
+    }
 
     if (!levels) {
         return <main style={{ padding: 24 }}>Lade…</main>;
@@ -31,12 +51,33 @@ export default function Game() {
 
     const firstLevel = levels[0];
 
-    return (
-        <Easy
-            title="Press & Regret: Easy"
-            levelNumber={firstLevel.number}
-            buttonText={firstLevel.instruction}
-            timer={45}
-        />
-    );
+    if(difficulty === "easy"){
+        return(
+            <Easy title={"Press & Regret: Easy"}
+                  levelNumber={firstLevel.number}
+                  buttonText={firstLevel.instruction}
+                  timer={45}
+            />
+        );
+    }
+    if(difficulty === "medium"){
+        return(
+            <Medium title={"Press & Regret: Medium"}
+                  levelNumber={firstLevel.number}
+                  buttonText={firstLevel.instruction}
+                  timer={30}
+            />
+        );
+    }
+    if(difficulty === "hard"){
+        return(
+            <Hard title={"Press & Regret: Hard"}
+                  levelNumber={firstLevel.number}
+                  buttonText={firstLevel.instruction}
+                  timer={15}
+            />
+        );
+    }
+
+    return <Navigate to="/game/easy" replace />;
 }
